4bdb66e442c9ed8d04feca7bbb3540db10b02914
Lorin Kurz <wi21b097@technikum-wien.at>
2022-06-22 15:26:44 +0200

    Initial commit

---
+++ src/main/java/com/example/weightconverter/WeightConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.weightconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weightconverter/WeightConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weightconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
