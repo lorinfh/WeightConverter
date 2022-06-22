ab8de18db0aa3d07b4c61ee3f3c0bd4c0a2a3cc6
Lorin Kurz <wi21b097@technikum-wien.at>
2022-06-22 15:57:43 +0200

    Added WeightConverter + WeightConverterController + WeightConverterTest and implemented Function + Test for "ouncetogram"

---
+++ src/main/java/com/example/weightconverter/WeightConverterController.java
@@ -0,0 +1,16 @@
+package com.example.weightconverter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeightConverterController {
+    WeightConverter wc = new WeightConverter();
+
+    @RequestMapping("/gram")
+    public double ouncetogram(@RequestParam double ounce){
+        return wc.ounceToGram(ounce);
+    }
+
+}
